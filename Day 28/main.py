683a8497b0ab5160cb2b5f70c0563ff9e97a2bdb
KevinBondioni <kevinbondioni@gmail.com>
2026-07-10 19:57:08 +0200

    feat: day 28 - implement countdown and dynamic time formatting

--- Day 28/main.py
+++ Day 28/main.py
@@ -1,3 +1,4 @@
+import math
 from tkinter import *
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -8,28 +9,66 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps=0
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
+def start_timer():
+    global reps
+    reps +=1 
+
+    work_sec=WORK_MIN*60
+    short_breack_sec=SHORT_BREAK_MIN*60
+    long_breack_sec=LONG_BREAK_MIN*60
+    
+    if reps % 8 ==0:
+        count_down(long_breack_sec)
+        title.config(text="Long Break",fg=PINK)
+
+    elif reps % 2==0:
+        count_down(short_breack_sec)
+        title.config(text="Break",fg=RED)
+    else:
+        count_down(work_sec)
+        title.config(text="Work",fg=GREEN)
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+def count_down(count):
+
+    count_min= math.floor(count / 60)
+    count_sec = count % 60 
+    if count_sec<10:
+        count_sec= f"0{count_sec}"
+    
+    canvas.itemconfig(timer_text, text= f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, count_down, count-1 )
+    else:
+        start_timer()
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window= Tk()
 window.title("Pomodoro App")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+
 # Create canvas
 canvas= Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img= PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 27, "bold"))
+timer_text= canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 27, "bold"))
 canvas.grid(column=1, row=1)
 
+
+
 # Create buttons
-start_button= Button(text="Start", highlightthickness=0)
+start_button= Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
+
 
 reset_button= Button(text="Reset", highlightthickness=0)
 reset_button.grid(column=2, row=2)
