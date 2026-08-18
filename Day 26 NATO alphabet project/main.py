a91ea1c1de6df7a3dbcb239637aa23eb9a430ed5
KevinBondioni <kevinbondioni@gmail.com>
2026-08-18 09:39:25 +0200

    feat(day-26): implement NATO alphabet project

--- Day 26 NATO alphabet project/main.py
+++ Day 26 NATO alphabet project/main.py
@@ -4,16 +4,20 @@ import pandas
 # Import CSV file and create a dataframe
 data=pandas.read_csv("Day 26 NATO alphabet project/nato_phonetic_alphabet.csv")
 
-#TODO 1. Create a dictionary in this format: {"A": "Alfa", "B": "Bravo"}
-
 # Loop through dataframe and create new dict 
 data_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+def word_list():
+    # Ask a word and create its list of letters
+    word=input("Enter a word: ").upper()
+    try:
+        phonetic_word_list=[data_dict[letter].title() for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")       
+        word_list()
+    else:
+        print(phonetic_word_list)
 
-# Ask a word and create its list of letters
-word=input("Enter a word: ").upper()
+word_list()
 
-# Create a list with the phonetical words
-phonetic_word_list=[data_dict[letter].title() for letter in word]
-print(phonetic_word_list)
+
